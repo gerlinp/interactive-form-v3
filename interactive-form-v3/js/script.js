b8d00cc010d1af6acba6dc2fa845feaad5f81bc0
gerlinp <gerlin.pierrelouis@gmail.com>
2021-06-19 03:16:07 -0400

    Tshirt Info

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,4 +1,9 @@
+// Global Variables
 const otherJob = document.querySelector('#other-job-role');
+const design = document.querySelector('#design');
+const color = document.querySelector('#color');
+const jsPuns = document.querySelectorAll('[data-theme="js puns"]')
+const jshearts = document.querySelectorAll('[data-theme="heart js"]')
 let title = document.querySelector('#title');
 
 // Name field focus on startup function
@@ -6,19 +11,62 @@ focusName = function() {
     document.querySelector('#name').focus()
 }
 
-// Event Listener for Job role.
-title.addEventListener('click', function() {
-    jobCheck();
-});
-
 // Adds other job field when other is selected.
-jobCheck = () => {
+ jobCheck = () => {
     if ( title.value !== 'other' ) {
         otherJob.style.display = "none";
     } else {
         otherJob.style.display = "block";
     };
 }
+// Adds Color field when a design is selected
+colorCheck = () => {
+    if ( design.value == 'Select Theme' ) {
+        color.disabled = true;
+    } else {
+        color.disabled = false;
+        for (i = 0; i < color.length; i++) {
+            if (design.value == 'js puns') {
+                designColor('heart js');
+                color.selectedIndex = '1';
+            } else {
+                designColor('js puns');
+                color.selectedIndex = '4';
+            }                
+        };
+    };
+};
+// checks data them and disables as neccessary.
+function designColor(val) {
+    if (color[i].getAttribute('data-theme') == val) {
+        color[i].disabled = true;
+    } else {
+        color[i].disabled = false;
+    }; 
+};
 
+
+
+
+
+// ------Event listeners---------//
+
+// Event Listener for Job role.
+title.addEventListener('change', () => {
+    jobCheck();
+});
+// Event listener for tshirt color.
+design.addEventListener('change', () => {
+    colorCheck();
+});
+
+
+
+
+
+
+
+
+colorCheck();
 focusName();
 jobCheck();
